474551a255c22753578bf620c0e84c57022ab48b
MrFailNote <145904338+MrFailNote@users.noreply.github.com>
2023-10-28 19:08:01 +0700

    Update Prirodik.py

--- Prirodik.py
+++ Prirodik.py
@@ -1,1 +1,11 @@
+import discord, os, requests
+import random as r
+from discord.ext import commands
 
+from Gen_logic import *
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+bot = commands.Bot(command_prefix='!', intents=intents)
