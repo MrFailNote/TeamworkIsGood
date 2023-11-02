48df2f44ef030f2ea1b5349d9db37d85ca42179c
MrFailNote <145904338+MrFailNote@users.noreply.github.com>
2023-11-02 15:28:14 +0700

    Update Prirodik.py

--- Prirodik.py
+++ Prirodik.py
@@ -14,61 +14,61 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def decay(ctx):
     item = r.randint(1,9)
     if item == 1:
-        with open(f'EnviPics/1', 'rb') as f:
+        with open(f'EnviPics/1.jpeg', 'rb') as f:
             picture = discord.File(f)
         infotext = '''Электрические батарейки:
-        Разлагаются в течении: около 200 лет
-        Вред для природы: тяжёлые металлы в батарейках крайне ядовиты для почвы и не только!'''
+    Разлагаются в течении: около 200 лет
+    Вред для природы: тяжёлые металлы в батарейках крайне ядовиты для почвы и не только!'''
     elif item == 2:
-        with open(f'EnviPics/2', 'rb') as f:
+        with open(f'EnviPics/2.jpg', 'rb') as f:
             picture = discord.File(f)
         infotext = '''Жевательная резинка:
-        Разлагается в течении: до 30 лет
-        Вред для природы: может вредить жизни многих животных, преимущественно мелких'''
+    Разлагается в течении: до 30 лет
+    Вред для природы: может вредить жизни многих животных, преимущественно мелких'''
     elif item == 3:
-        with open(f'EnviPics/3', 'rb') as f:
+        with open(f'EnviPics/3.jpg', 'rb') as f:
             picture = discord.File(f)
         infotext = '''Фольга:
-        Разлагается в течении: в среднем до 100 лет
-        Вред для природы: почти минимален'''
+    Разлагается в течении: в среднем до 100 лет
+    Вред для природы: почти минимален'''
     elif item == 4:
-        with open(f'EnviPics/4', 'rb') as f:
+        with open(f'EnviPics/4.jpg', 'rb') as f:
             picture = discord.File(f)
         infotext = '''Книги:
-        Разлагаются в течении: 1 - 3 месяцев
-        Вред для природы: сами по себе безопасны, однако краска может быть токсична'''
+    Разлагаются в течении: 1 - 3 месяцев
+    Вред для природы: сами по себе безопасны, однако краска может быть токсична'''
     elif item == 5:
-        with open(f'EnviPics/5', 'rb') as f:
+        with open(f'EnviPics/5.jpg', 'rb') as f:
             picture = discord.File(f)
         infotext = '''Газеты:
-        Разлагаются в течении: порядка 1 - 3 месяцев 
-        Вред для природы: Краска содержитв себе токсичные материалы'''
+    Разлагаются в течении: порядка 1 - 3 месяцев 
+    Вред для природы: Краска содержитв себе токсичные материалы'''
     elif item == 6:
-        with open(f'EnviPics/6', 'rb') as f:
+        with open(f'EnviPics/6.jpg', 'rb') as f:
             picture = discord.File(f)
         infotext = '''Рыболовная леска:
-        Разлагается в течении: около 600 лет
-        Вред для природы: содержит ядовитые вещества, может вредить животным.'''
+    Разлагается в течении: около 600 лет
+    Вред для природы: содержит ядовитые вещества, может вредить животным.'''
     elif item == 7:
-        with open(f'EnviPics/7', 'rb') as f:
+        with open(f'EnviPics/7.jpg', 'rb') as f:
             picture = discord.File(f)
         infotext = '''Стекло:
-        Разлагается в течении: Более 1000 лет 
-        Вред для природы: Почти не разлагаясь могут ранить животных и человека'''
+    Разлагается в течении: Более 1000 лет 
+    Вред для природы: Почти не разлагаясь могут ранить животных и человека'''
     elif item == 8:
-        with open(f'EnviPics/8', 'rb') as f:
+        with open(f'EnviPics/8.jpg', 'rb') as f:
             picture = discord.File(f)
         infotext = '''Огрызки фруктов:
-        Разлагаются в течении: до 2 месяцев
-        Вред для природы: нет, могут работать как естественный компост'''
+    Разлагаются в течении: до 2 месяцев
+    Вред для природы: нет, могут работать как естественный компост'''
     elif item == 9:
-        with open(f'EnviPics/9', 'rb') as f:
+        with open(f'EnviPics/9.png', 'rb') as f:
             picture = discord.File(f)
         infotext = '''Туалетная бумага:
-        Разлагается в течении: 2 - 4 дней 
-        Вред для природы: нет'''
+    Разлагается в течении: 2 - 4 дней 
+    Вред для природы: нет'''
         
     await ctx.send(infotext)
     await ctx.send(file=picture)
     
-bot.run("k3T4s_ggHHxMdV5A1UrDLaqc1v3Qk9Sv")
+bot.run("MTE2Nzc4OTYwNTQ4Mzk3NDY5OA.GN_i2S.lU61QNtqsXFTS78z5j_W6bIf2q6z7SxEx9P22s")
